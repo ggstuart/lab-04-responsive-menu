9ae1990533153793578d165afb73ad15bd5009f9
Graeme Stuart <gstuar02@LEC-ADMIN.dmu.ac.uk>
2020-02-13 08:59:26 +0000

    basic working version

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = () => { menuNav.classList.toggle('open')}
+
+menuToggle.addEventListener('click', toggleMenu);
+
+
+function toggleHighlight() {
+  title.classList.toggle('highlight');
+}
+
+title.addEventListener('click', toggleHighlight);
